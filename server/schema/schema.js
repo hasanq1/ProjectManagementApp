78aaf0a584bd99dfc279c8beea1bb8c709f398f9
Hasan Qureshi <hasanq36@gmail.com>
2022-11-16 23:14:01 -0600

    Git ignore added and database connection established with working queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,7 @@
- const {projects, clients } = require('../sampleData.js');
+//  const {projects, clients } = require('../sampleData.js');
+// Import mongoose models after creating them
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
  const {
     GraphQLObjectType, 
@@ -20,7 +23,7 @@
         client: {
             type: ClientType,
             resolve(parent, args) {
-                return clients.find(client => client.id === parent.clientId)
+                return client.findById(parent.clientId)
             }
         }
     })
@@ -44,28 +47,28 @@
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args){
-            return projects;
+            return Project.find();
             },
         },
         project:{
         type: ProjectType,
         args: { id: { type :GraphQLID}}, // method to loop and find the arg values
         resolve(parent, args){
-            return projects.find (project => project.id === args.id);
+            return Project.findById(args.id);
             },
         },
         //Client query 
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args){
-            return clients;
+            return Client.find();
             },
         },
         client:{
         type: ClientType,
         args: { id: { type :GraphQLID}}, // method to loop and find the arg values
         resolve(parent, args){
-            return clients.find (client => client.id === args.id);
+            return Client.findById(args.id);
             },
         },
     }
